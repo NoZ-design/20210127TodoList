5e0bf58986eef9e46d64f3edaa0b58fbe8120838
Yu Xuan Lin <68496572+NoZ-design@users.noreply.github.com>
2021-01-27 23:03:39 +0800

    20210127TodoList

---
+++ result.js
@@ -0,0 +1,22 @@
+let input = prompt("What would you like to do?")
+const todos = ['Collect Chicken Eggs', 'Clean Litter Box']
+
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("***********")
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`)
+    }
+    console.log("***********")
+  } else if (input === "new") {
+    const newTodo = prompt("OK.What is the new todo?")
+    todos.push(newTodo)
+    console.log(`${newTodo} added to the list.`)
+  } else if (input === "delete") {
+    const index = prompt("OK.Enter an index to delete.")
+    const deletes = todos.splice(index, 1)
+    console.log(`OK.Deleted ${deletes}`)
+  }
+  input = prompt("What would you like to do?")
+}
+console.log("OK,QUIT THIS APP.")
